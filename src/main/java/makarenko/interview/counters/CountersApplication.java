fb130ff7c07f3d6514cf2939006cd516330a5c2e
Alexey Makarenko <alexey-makarenko@hotmail.com>
2020-03-15 20:03:16 +0300

    Initial project

---
+++ src/main/java/makarenko/interview/counters/CountersApplication.java
@@ -0,0 +1,13 @@
+package makarenko.interview.counters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CountersApplication.class, args);
+    }
+
+}
---
+++ src/test/java/makarenko/interview/counters/CountersIntegrationTest.java
@@ -0,0 +1,13 @@
+package makarenko.interview.counters;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountersIntegrationTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
